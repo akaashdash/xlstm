bfc407e397e52de0f0749da0479dcfcd0569f24d
akaashdash <akaash.dash@gmail.com>
2024-05-08 13:09:08 -0400

    add exponential stability, tweak forward pass

--- xlstm.py
+++ xlstm.py
@@ -17,17 +17,21 @@ class sLSTM(nn.Module):
         self.Rf = nn.Linear(hidden_size, hidden_size)
         self.Ro = nn.Linear(hidden_size, hidden_size)
 
-    def forward(self, x, h_prev, c_prev, n_prev):
+    def forward(self, x, h_prev, c_prev, n_prev, m_prev):
         z = torch.tanh(self.Wz(x) + self.Rz(h_prev))
-        i = torch.exp(self.Wi(x) + self.Ri(h_prev))
-        f = torch.sigmoid(self.Wf(x) + self.Rf(h_prev)) # could also be exp
         o = torch.sigmoid(self.Wo(x) + self.Ro(h_prev))
+
+        i_tilde = self.Wi(x) + self.Ri(h_prev)
+        f_tilde = self.Wf(x) + self.Rf(h_prev)
+        m_t = torch.max(f_tilde + m_prev, i_tilde)
+        i = torch.exp(i_tilde - m_t)
+        f = torch.exp(f_tilde + m_prev - m_t)
         
         c_t = f * c_prev + i * z
         n_t = f * n_prev + i
-        h_t = o * c_t / n_t
+        h_t = o * c_t / n_t # A.2 says n_t^{-1} ??
         
-        return h_t, c_t, n_t
+        return h_t, c_t, n_t, m_t
     
 class mLSTM(nn.Module):
     def __init__(self, input_size, hidden_size):
@@ -42,28 +46,32 @@ class mLSTM(nn.Module):
         self.Wf = nn.Linear(input_size, hidden_size)
         self.Wo = nn.Linear(input_size, hidden_size)
 
-    def forward(self, x, h_prev, c_prev, n_prev):
+    def forward(self, x, h_prev, c_prev, n_prev, m_prev): # Parallelize according to A.3?
         q = self.Wq(x)
         k = self.Wk(x) / (self.hidden_size ** 0.5)
         v = self.Wv(x)
         
-        i = torch.exp(self.Wi(x))
-        f = torch.sigmoid(self.Wf(x))  # could also be exp
         o = torch.sigmoid(self.Wo(x))
+
+        i_tilde = self.Wi(x)
+        f_tilde = self.Wf(x)
+        m_t = torch.max(f_tilde + m_prev, i_tilde)
+        i = torch.exp(i_tilde - m_t)
+        f = torch.exp(f_tilde + m_prev - m_t)
         
         c_t = f * c_prev + i * torch.outer(v, k)
         n_t = f * n_prev + i * k
         h_t = o * torch.matmul(c_t, q) / torch.max(torch.abs(torch.matmul(n_t.T, q)), 1)
         
-        return h_t, c_t, n_t
+        return h_t, c_t, n_t, m_t
     
 class xLSTMBlock(nn.Module):
-    def __init__(self, input_size, hidden_size, num_heads, block_type='post'):
+    def __init__(self, input_size, hidden_size, block_type='post'):
         super(xLSTMBlock, self).__init__()
         self.block_type = block_type
         
         if block_type == 'post':
-            self.sLSTM = sLSTM(input_size, hidden_size, num_heads)
+            self.sLSTM = sLSTM(input_size, hidden_size)
             self.ff = nn.Sequential(
                 nn.Linear(hidden_size, 4 * hidden_size),
                 nn.GELU(),
@@ -76,36 +84,42 @@ class xLSTMBlock(nn.Module):
         else:
             raise ValueError('Invalid block type.')
         
-    def forward(self, x, h_prev=None):
+    def forward(self, x, h_prev, c_prev, n_prev, m_prev):
         if self.block_type == 'post':
-            h_t, c_t, n_t= self.sLSTM(x, h_prev)
+            h_t, c_t, n_t, m_t = self.sLSTM(x, h_prev, c_prev, n_prev, m_prev)
             h_t = self.ff(h_t)
         elif self.block_type == 'pre':
             x = self.ff1(x)
-            h_t, c_t, n_t = self.mLSTM(x)
+            h_t, c_t, n_t, m_t = self.mLSTM(x, h_prev, c_prev, n_prev, m_prev)
             h_t = self.ff2(h_t)
         
-        return h_t, c_t, n_t
+        return h_t, c_t, n_t, m_t
     
 class xLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_heads, block_ratio):
+    def __init__(self, input_size, hidden_size, num_layers, block_ratio):
         super(xLSTM, self).__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.num_heads = num_heads
         self.block_ratio = block_ratio
         
         self.blocks = nn.ModuleList()
         for i in range(num_layers):
             if i < block_ratio * num_layers:
-                block = xLSTMBlock(input_size, hidden_size, num_heads, block_type='post')
+                block = xLSTMBlock(input_size, hidden_size, block_type='post')
             else:
-                block = xLSTMBlock(input_size, hidden_size, num_heads, block_type='pre')
+                block = xLSTMBlock(input_size, hidden_size, block_type='pre')
             self.blocks.append(block)
 
     def forward(self, x):
-        h_= x
-        for block in self.blocks:
-            h, c, n = block(h)
+        # This makes sense to me, input size must match block size for continuous production
+        # Not sure how this forward pass aligns with the paper
+        assert len(x) == len(self.blocks)
+        # No idea how to initialize these tbh
+        h = torch.zeros(self.hidden_size)
+        c = torch.zeros(self.hidden_size)
+        n = torch.zeros(self.hidden_size)
+        m = torch.zeros(self.hidden_size)
+        for i in range(len(x)):
+            h, c, n, m = self.blocks[i](x[i], h, c, n, m)
         return h
